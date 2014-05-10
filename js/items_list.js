6600aacba83af3e2a6f9b38032d3b6644ef48a6e
Karen Lees <karenlees@google.com>
2014-05-09 17:30:55 -0700

    Adds icon and title to behavior overlay.

--- js/items_list.js
+++ js/items_list.js
@@ -385,7 +385,9 @@ cr.define('apps_dev_tool', function() {
     setBehaviorLink_: function(item, el) {
       var behavior = el.querySelector('.behavior-link');
       behavior.addEventListener('click', function(e) {
-        AppsDevTool.showOverlay($('behaviorOverlay'));
+         $('behavior-extension-icon').style.backgroundImage = 'url(' + item.icon_url + ')';
+         $('behavior-extension-title').textContent = item.name;
+         AppsDevTool.showOverlay($('behaviorOverlay'));
       });
     },
 
