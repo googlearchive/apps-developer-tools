d18103dae12020908017825a58e6b74eda97b088
pmarch <pmarch@google.com>
2014-05-12 15:26:07 -0400

    .

---
+++ js/delete_all_behavior_overlay.js
@@ -0,0 +1,59 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+cr.define('apps_dev_tool', function() {
+  'use strict';
+
+/** const */ var AppsDevTool = apps_dev_tool.AppsDevTool;
+
+  /**
+   * DeleteAllBehaviorOverlay class
+   * Encapsulated handling of the 'Delete All Behavior History' overlay page.
+   * @constructor
+   */
+  function DeleteAllBehaviorOverlay() {}
+
+  cr.addSingletonGetter(DeleteAllBehaviorOverlay);
+
+  DeleteAllBehaviorOverlay.prototype = {
+    initializePage: function() {
+      var overlay = $('overlay');
+      cr.ui.overlay.setupOverlay(overlay);
+      cr.ui.overlay.globalInitialization();
+      overlay.addEventListener('cancelOverlay', this.hideOverlay_.bind(this));
+
+      $('delete-all-behavior-dismiss').addEventListener('click',
+          this.hideOverlay_.bind(this));
+      $('delete-all-behavior-commit').addEventListener('click',
+          this.handleCommit_.bind(this));
+    },
+
+    /**
+     * Hides the present overlay showing.
+     * @private
+     */
+    hideOverlay_: function() {
+      AppsDevTool.showOverlay(null);
+    },
+
+    /**
+     * Handles a click on the delete all behavior history.
+     * @param {Event} e The click event.
+     * @private
+     */
+    handleCommit_: function(e) {
+      if ($('delete-all-urls-radio').checked) {
+        chrome.activityLogPrivate.deleteUrls([]);
+      } else if ($('delete-database-radio').checked) {
+        chrome.activityLogPrivate.deleteDatabase();
+      }
+      this.hideOverlay_();
+    },
+  };
+
+  // Export
+  return {
+    DeleteAllBehaviorOverlay: DeleteAllBehaviorOverlay,
+  };
+});
